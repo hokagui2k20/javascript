550163853fbac6563603634bc33dc404b8a9bad7
Guilherme Quarema <111399586+hokagui2k20@users.noreply.github.com>
2022-12-21 22:52:15 -0300

    comit

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 99
+console.log(`seu veiculo esta a ${vel}Km/h`)
+if (vel > 60){
+    console.log('Voce ultrapassou a velocidade limite, MULTADO!!!')
+}
+console.log('Dirija sempre usando cinto de segurança!!!')
