80e5f20198841e49e48dd2190769c6d6b3ae8b04
Guilherme Quarema <111399586+hokagui2k20@users.noreply.github.com>
2022-12-28 21:16:50 -0300

    finish

--- Desafio1/modelo/script.js
+++ Desafio1/modelo/script.js
@@ -6,7 +6,44 @@ var res = document.querySelector('div#res')
 if (fano.value.length == 0 || fano.value > ano) {
     window.alert('ERRO PANI NO SISTEMA ALGUEM NE DESCONFIGUROU')
 } else {
-    
-}
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        genero = 'você é um corno'
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'nene2.png')
+        } else if (idade < 21) {
+            //jovem
+            img.setAttribute('src','men.png')
+        } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'homi.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'veio.png')
+        }
+    } else if (fsex[1].checked) {
+        genero = 'você é uma trouxa'
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'nene.png')
+        } else if (idade < 21) {
+            //jovem
+            img.setAttribute('src','muie.png')
+        } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'muiednv.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'veia')
+        }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Chegamos a conclusão de que ${genero} aos ${idade} anos!!`
+    res.appendChild(img) }
 
 }
